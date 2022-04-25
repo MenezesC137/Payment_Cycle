910f5a8cac9718c43f93bb5425d0624adad3cec6
MenezesC137 <iamcarloseduardo@hotmail.com>
2022-04-25 12:03:54 -0300

    Usando o ValueBox

--- frontend/src/common/template/menu.jsx
+++ frontend/src/common/template/menu.jsx
@@ -3,7 +3,7 @@ import MenuItem from "./menuItem";
 import MenuTree from "./menuTree";
 
 export default props => (
-    <ul className="siderbar-menu">
+    <ul className="sidebar-menu">
         <MenuItem path='#' label='Dashboard' icon='dashboard' />
         <MenuTree label='Cadastro' icon='edit'>
             <MenuItem path='#billingCycles' 
