3f37e60ed7429b5f91eb812687c8722cdcde149b
MenezesC137 <iamcarloseduardo@hotmail.com>
2022-04-26 08:28:55 -0300

    Estrutura do ciclo

--- frontend/src/common/template/footer.jsx
+++ frontend/src/common/template/footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default props => (
     <footer className="main-footer">
         <strong>
-            Copyright &copy; 2017
+            Copyright &copy; 2022
             <a href="https://github.com/MenezesC137/Payment_Cycle" target='_blank'> Repositorio</a>
         </strong>
     </footer>
