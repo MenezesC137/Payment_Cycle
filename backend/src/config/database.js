b42ba2c60d3912da0bc03b8f384e37409dd474fc
Carlos <iamcarloseduardo@hotmail.com>
2022-04-28 18:37:59 -0300

    E pra ta funcionando a autenticação

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,13 +1,12 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
-module.exports = mongoose.connect("mongodb://localhost/mymoney", {
-  useNewUrlParser: true,
-});
 
-mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatorio";
-mongoose.Error.messages.Number.min =
-  "O '{VALUE}' informado é menos que o limite mínimo de '{MIN}'";
-mongoose.Error.messages.Number.max =
-  "O '{VALUE}' informado é maior que o limite máximo de '{MAX}'";
-mongoose.Error.messages.String.enum =
-  "'{VALUE}' não é valido para o atributo '{PATH}'";
+module.exports = mongoose.connect('mongodb://localhost:27017/mymoney', { useNewUrlParser: true,  useMongoClient: true })
+
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório.";
+mongoose.Error.messages.Number.min = 
+    "O '{VALUE}' informando é menor que o limite mínimo de '{MIN}'.";
+mongoose.Error.messages.Number.max = 
+    "O '{VALUE}' informando é maior que o limite máximo de '{MAX}'.";
+mongoose.Error.messages.String.enum = 
+    "'{VALUE}' não é válido para o atributo '{PATH}'.";
